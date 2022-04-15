df52d796826c048890e8702f313066d914aa02df
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-04-15 12:49:31 -0600

    Complete new gameplay & some DOM manipulation

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let round = 1;
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+
 function getNumberFromRange(min, max) {
     return Math.floor(Math.random() * (max - min +1)) + min;
 }
@@ -5,50 +11,71 @@ function getNumberFromRange(min, max) {
 function computerChoice() {
     let choice = getNumberFromRange(1, 3);
 
-    console.log(choice)
-
     switch (choice) {
         case 1: 
-            return "rock";
+            return 'rock';
             break;
         case 2: 
-            return "paper";
+            return 'paper';
             break;
         case 3:
-            return "scissors";
+            return 'scissors';
             break;
     }
 }
 
-function playerChoice() {
-    const rock = document.querySelector('#rock');
-    const paper = document.querySelector('#paper');
-    const scissors = document.querySelector('#scissors');
-
-    rock.addEventListener('click', () => {
-        return rock
-    });
-    paper.addEventListener('click', () => {
-        return 'paper'
-    });
-    scissors.addEventListener('click', () => {
-        return 'scissors'
-    });
-}
 function playRound(playerSelection, computerSelection) {
 
+    console.log('clicked')
+
     if (playerSelection == computerSelection){
-        return "tie";
-    }else if (playerSelection=="rock" && computerSelection=="scissors"){
-        return "player";
-    } else if (playerSelection == "paper" && computerSelection == "rock"){
-        return "player";
-    } else if(playerSelection == "scissors" && computerSelection == "paper"){
-        return "player";
+        setScore('tie');
+    }else if (playerSelection=='rock' && computerSelection=='scissors'){
+        setScore('player');
+    } else if (playerSelection == 'paper' && computerSelection == 'rock'){
+        setScore('player');
+    } else if(playerSelection == 'scissors' && computerSelection == 'paper'){
+        setScore('player');
     } else {
-        return "computer";
+        setScore('computer');
     }
 }
 
+function setScore(result){
+    let playerScore = 0;
+    let computerScore = 0;
+    const player = document.querySelector('#player');
+    const computer = document.querySelector('#computer');
 
-playerChoice()
+        if(result == 'player'){
+            playerScore++;
+            player.querySelector('.score').textContent = playerScore;
+        } else if(result == 'computer'){
+            computerScore++;
+            computer.querySelector('.score').textContent = computerScore;
+        }
+       round++
+       console.log(round)
+}
+
+rock.addEventListener('click', () => {
+    if(round <= 5){
+        playRound('rock', computerChoice())
+    }else{
+        alert('The game is over!')
+    }
+});
+paper.addEventListener('click', () => {
+    if(round <= 5){
+        playRound('paper', computerChoice())
+    }else{
+        alert('The game is over!')
+    }
+});
+scissors.addEventListener('click', () => {
+    if(round <= 5){
+        playRound('scissors', computerChoice())
+    }else{
+        alert('The game is over!')
+    }
+});
